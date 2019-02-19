00ad032dc2159ce24adc96fbd166caa0bda74581
Micah Caffey <mrcaffey@gmail.com>
2019-02-18 18:17:58 -0700

    use netlify-gatsby form handling

--- src/pages/index.js
+++ src/pages/index.js
@@ -127,7 +127,7 @@ class HomeIndex extends React.Component {
                         <p>I am quite responsive via email, so please do not hesitate to contact me for my services!</p>
                         <div className="row">
                             <div className="8u 12u$(small)">
-                                <form method="post" action="#">
+                                <form name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field" action="#">
                                     <div className="row uniform 50%">
                                         <div className="6u 12u$(xsmall)"><input type="text" name="name" id="name" placeholder="Name" /></div>
                                         <div className="6u 12u$(xsmall)"><input type="email" name="email" id="email" placeholder="Email" /></div>
