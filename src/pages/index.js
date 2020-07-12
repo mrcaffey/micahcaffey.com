27b5950143abd849039853a1affef6c8cecc95f3
Micah Caffey <micah@paydrt.com>
2020-07-12 13:25:47 -0600

    update info for 2020

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,9 +10,9 @@ import thumb02 from '../assets/images/thumbs/02.jpg'
 import thumb03 from '../assets/images/thumbs/03.jpg'
 
 
-import full01 from '../assets/images/fulls/01.jpg'
-import full02 from '../assets/images/fulls/02.jpg'
-import full03 from '../assets/images/fulls/03.jpg'
+import paydrt from '../assets/images/fulls/paydrt.png'
+import fabriq from '../assets/images/fulls/fabriq.jpg'
+import knx from '../assets/images/fulls/knx.png'
 
 import htmlicon from '../assets/images/icons/icons8-html-5-96.png'
 import cssicon from '../assets/images/icons/icons8-css3-96.png'
@@ -26,12 +26,12 @@ import sqlicon from '../assets/images/icons/icon-sql-96.png'
 
 
 const DEFAULT_IMAGES = [
-    { id: '1', src: full01, thumbnail: thumb01, caption: 'Enterprise International',
-    description: 'American travel site for International business clientele.' },
-    { id: '2', src: full02, thumbnail: thumb02, caption: 'Caffey, Inc.', 
-    description: 'Design-Build Medical Construction website.'},
-    { id: '3', src: full03, thumbnail: thumb03, caption: 'Mugshot', 
-    description: 'Facebook clone for those who prefer jumpsuits.'},
+    { id: '1', src: paydrt, caption: 'Paydrt',
+    description: 'Business Management Web App for clients to add and track their social media and site engagement statistics utilizing various API services & Hubspot. Bought by local marketing advertising firm Monumetric. LINK: https://paydrt.com/' },
+    { id: '2', src: fabriq, caption: 'FabrIQ', 
+    description: 'Desktop app for Windows, MacOS, & Linux built on Electron.js & React in which content creators, freelancers and remote workers are able to separate their work and passion projects into Workspaces. User has the ability to add thousands of Apps from App Store to their Workspace and to customize their URL as well as utilize split-screen web browsing & app switching.'},
+    { id: '3', src: knx, caption: 'KNX', 
+    description: 'Web application in which clients scan user-generated QR code and are directed to application which gathers information about requested services. This client data is sent to user data tables to keep track of customers per user. '},
 ];
 
 class HomeIndex extends React.Component {
@@ -81,7 +81,7 @@ class HomeIndex extends React.Component {
     }
 
     render() {
-        const siteTitle = "Micah Caffey: Software Developer"
+        const siteTitle = "Micah Caffey: Web Developer"
         const siteDescription = "Portfolio Site"
 
         return (
@@ -97,37 +97,42 @@ class HomeIndex extends React.Component {
                         <header className="major">
                             <h1>Welcome! </h1>
                         </header>
-                        <p>I am a freelance Full Stack web developer passionate about using programming to solve problems and deliver quality products. I also have experience in Project Management and experience implementing new software, hardware and managing multiple client relations and teams.
+                        <p>I am a Developer passionate about using programming to solve problems and deliver quality products. I also have experience in Project Management including implementing new software, hardware and managing multiple client relations and teams.
                            When I'm not working on a project, I enjoy hiking, exercising, cooking and photography. </p>
                         <br/>
                         <br/>
-                        <p>Currently, I work with mainly JavaScript, HTML/CSS/Bootstrap, React and React Native on the front end with 
-                           NodeJS and Ruby on Rails for the backend.</p>
+                        <p>The majority of my work is with Javascript, both client and server side, and I do enjoy it. However, I do have exposure to Python & Ruby as well. I
+                            have worked with tools such as Amazon Firebase, Google Cloud Console, Payment Processing API's (Stripe, Paypal) as well as set up of user
+                            authentication methods such as Facebook, Google, Microsoft & Apple. 
+                        </p>
                         <br/>
                         <br/>
-                        <img src={htmlicon} alt="HTML"/>
-                        <img src={cssicon} alt="CSS"/>
-                        <img src={jsicon} alt="JavaScript"/>
-                        <img src={reacticon} alt="React"/>
-                        <br/>
-                        <img src={nodeicon} alt="Node"/>
-                        &emsp;
-                        <img src={rubyicon} alt="Ruby"/>
-                        &emsp;
-                        <img src={railsicon} alt="Rails"/>
-                        &emsp;
-                        <img src={phpicon} alt="PHP"/>
-                        &emsp;
-                        <img src={sqlicon} alt="SQL"/>
+                        <h1>Frontend Technologies:</h1>
+                        <li>React</li>
+                        <li>Redux</li>
+                        <li>TypeScript</li>
+                        <li>RESTful API's</li>
+                        <li>HTML5</li>
+                        <li>CSS3/SASS</li>
                         <br/>
                         <br/>
+                        <h1>Backend Technologies:</h1>
+                        <li>Node</li>
+                        <li>Express</li>
+                        <li>TypeScript</li>
+                        <li>Knex.js</li>
+                        <br/>
+                        <br/>
+                        <h1>Database</h1>
+                        <li>PostgreSQL</li>
+                        <li>Google Cloud DB</li>
 
          
                         <ul className="actions">
                             <li><a href="https://github.com/mrcaffey" className="button">Check me out on Github</a></li>
                         </ul>
                         <ul className="actions">
-                            <li><a href="assets/images/fulls/resume.pdf" className="button" download>Download my Resume</a></li>
+                            <li><a href="assets/images/fulls/MicahCaffeyResume.pdf" className="button" download>Download my Resume</a></li>
                         </ul>
                     </section>
                
@@ -177,14 +182,8 @@ class HomeIndex extends React.Component {
                             <div className="4u 12u$(small)">
                                 <ul className="labeled-icons">
                                     <li>
-                                        <h3 className="icon fa-home"><span className="label">Address</span></h3>
-                                        55 500 W<br />
-                                        Salt Lake City, UT.<br />
-                                        United States
-                                    </li>
-                                    <li>
                                         <h3 className="icon fa-mobile"><span className="label">Phone</span></h3>
-                                        228-493-2200
+                                        801-707-5209
                                     </li>
                                     <li>
                                         <h3 className="icon fa-envelope-o"><span className="label">Email</span></h3>
